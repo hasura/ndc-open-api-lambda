6f1f2f4d36aa40c3c37f550efb75fe00d17eb62f
m-Bilal <mohd.bilal1996@gmail.com>
2024-04-25 13:18:27 +0530

    tests refactored

--- src/app/open-api-generator/parser/types-parser.test.ts
+++ src/app/open-api-generator/parser/types-parser.test.ts
@@ -11,55 +11,78 @@ type FunctionParams = {
   requireRelaxedTypeAnnotation: boolean;
 };
 
-const tests: {
+type TestCase = {
   name: string;
   oasFile: string;
   goldenFile: string;
   expected?: Map<string, FunctionParams>;
   outDir?: string;
-}[] = [
+};
+
+function setupTest(testCase: TestCase) {
+  const outDir = generateRandomDir(
+    path.resolve(__dirname, `./_temp/${testCase.name}`),
+  );
+  testCase.outDir = outDir;
+  testCase.oasFile = path.resolve(__dirname, testCase.oasFile);
+  testCase.goldenFile = path.resolve(__dirname, testCase.goldenFile);
+  const fileContent = fs.readFileSync(testCase.goldenFile, "utf8");
+  if (fileContent) {
+    try {
+      testCase.expected = new Map(Object.entries(JSON.parse(fileContent)));
+    } catch (e) {}
+  }
+}
+
+function cleanup(testCase: TestCase) {
+  // remove generated api.ts files
+  // comment to inspect the generated files
+  fs.rmSync(`${testCase.outDir}`, { recursive: true });
+}
+
+const queryParamTests: TestCase[] = [
   {
-    name: "GenerateParams_DemoBlogApi",
+    name: "GenerateQueryParams_DemoBlogApi",
     oasFile: "../tests/oas-docs/demo-blog-api.json",
     goldenFile: "./testdata/golden-files/query-tests/demo-blog-api.json",
   },
   {
-    name: "GenerateParams_GeomagApi",
+    name: "GenerateQueryParams_GeomagApi",
     oasFile: "../tests/oas-docs/geomag.json",
     goldenFile: "./testdata/golden-files/query-tests/geomag.json",
   },
   {
-    name: "GenerateParams_Petstore",
+    name: "GenerateQueryParams_Petstore",
     oasFile: "../tests/oas-docs/petstore.yaml",
     goldenFile: "./testdata/golden-files/query-tests/petstore.json",
   },
   {
-    name: "GenerateParams_GoogleAdsense",
+    name: "GenerateQueryParams_GoogleAdsense",
     oasFile: "../tests/oas-docs/google-adsense.json",
     goldenFile: "./testdata/golden-files/query-tests/google-adsense.json",
   },
   {
-    name: "GenerateParams_Instagram",
+    name: "GenerateQueryParams_Instagram",
     oasFile: "../tests/oas-docs/instagram.json",
     goldenFile: "./testdata/golden-files/query-tests/instagram.json",
   },
   {
-    name: "GenerateParams_Gitlab",
+    name: "GenerateQueryParams_Gitlab",
     oasFile: "../tests/oas-docs/gitlab.json",
     goldenFile: "./testdata/golden-files/query-tests/gitlab.json",
   },
   {
-    name: "GenerateParams_Dropbox",
+    name: "GenerateQueryParams_Dropbox",
     oasFile: "../tests/oas-docs/dropbox.json",
     goldenFile: "./testdata/golden-files/query-tests/dropbox.json",
   },
   {
-    name: "GenerateParams_Adobe",
+    name: "GenerateQueryParams_Adobe",
     oasFile: "../tests/oas-docs/adobe.json",
     goldenFile: "./testdata/golden-files/query-tests/adobe.json",
   },
   {
-    name: "GenerateParams_AwsAutoscaling",
+    name: "GenerateQueryParams_AwsAutoscaling",
     oasFile: "../tests/oas-docs/aws-autoscaling.json",
     goldenFile: "./testdata/golden-files/query-tests/aws-autoscaling.json",
   },
@@ -67,20 +90,9 @@ const tests: {
 
 describe("GenerateQueryParams", async () => {
   const templateDir = path.resolve(getTemplatesDirectory(), "./custom");
-  for (const testCase of tests) {
+  for (const testCase of queryParamTests) {
     before(function () {
-      const outDir = generateRandomDir(
-        path.resolve(__dirname, `./_temp/${testCase.name}`),
-      );
-      testCase.outDir = outDir;
-      testCase.oasFile = path.resolve(__dirname, testCase.oasFile);
-      testCase.goldenFile = path.resolve(__dirname, testCase.goldenFile);
-      const fileContent = fs.readFileSync(testCase.goldenFile, "utf8");
-      if (fileContent) {
-        try {
-          testCase.expected = new Map(Object.entries(JSON.parse(fileContent)));
-        } catch (e) {}
-      }
+      setupTest(testCase);
     });
 
     it(testCase.name, async () => {
@@ -120,9 +132,7 @@ describe("GenerateQueryParams", async () => {
     });
 
     after(function () {
-      // remove generated api.ts files
-      // comment to inspect the generated files
-      fs.rmSync(`${testCase.outDir}`, { recursive: true });
+      cleanup(testCase);
     });
   }
 });
