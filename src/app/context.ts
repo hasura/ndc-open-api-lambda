53a910c01bbed88570e70a27b9df3291ea381fd4
m-Bilal <mohd.bilal1996@gmail.com>
2024-05-16 09:54:57 +0530

    integrated context with update cli command and codegen

--- src/app/context.ts
+++ src/app/context.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import * as fs from "fs";
 import * as logger from "../util/logger";
 import { exit } from "process";
 
@@ -6,12 +7,15 @@ export function getInstance(): Context {
   return Context.getInstance();
 }
 
-export type Configuration = {
-  outputDir?: string;
+type Configuration = {
+  outputDirectory?: string;
   apiFileName: string;
   functionsFileName: string;
   logLevel?: LogLevel;
   prettyLogs: boolean;
+
+  openApiUri?: string;
+  overwriteFiles: boolean;
 };
 
 export enum LogLevel {
@@ -23,6 +27,21 @@ export enum LogLevel {
   FATAL = "fatal",
   PANIC = "panic"
 }
+
+const PACKAGE_JSON_FILENAME = 'package.json';
+const TS_CONFIG_FILENAME = 'tsconfig.json'
+
+const NODE_VERSION = 'node20';
+
+export const TS_CONFIG_FILE_CONTENT = `{
+  "extends": "./node_modules/@tsconfig/${NODE_VERSION}/tsconfig.json",
+  "compilerOptions": {
+    "lib": [
+      "dom"
+    ]
+  }
+}
+`;
 
 /**
  * Context is a singleton class that holds the configuration of the app
@@ -37,6 +56,7 @@ export class Context {
       functionsFileName: "functions.ts",
       logLevel: this.readLogLevel(),
       prettyLogs: process.env.NDC_OAS_LAMBDA_PRETTY_LOGS === 'true',
+      overwriteFiles: false,
     };
   }
 
@@ -59,7 +79,7 @@ export class Context {
 
   public setOutputDirectory(outputDir: string) {
     try {
-      this.config.outputDir = path.resolve(outputDir);
+      this.config.outputDirectory = path.resolve(outputDir);
     } catch (e) {
       logger.fatal(`Cannot resolve output directory path: ${path}`);
       logger.debug(e);
@@ -68,11 +88,11 @@ export class Context {
   }
 
   public getOutputDirectory(): string {
-    if (!this.config.outputDir) {
+    if (!this.config.outputDirectory) {
       logger.fatal("No output directory set");
       throw new Error("Output Directory is null");
     }
-    return this.config.outputDir;
+    return this.config.outputDirectory;
   }
 
   public getApiFileName(): string {
@@ -91,6 +111,14 @@ export class Context {
     return path.resolve(this.getOutputDirectory(), this.getFunctionsFileName());
   }
 
+  public getPackageJsonFilePath(): string {
+    return path.resolve(this.getOutputDirectory(), PACKAGE_JSON_FILENAME);
+  }
+
+  public getTsConfigFilePath(): string {
+    return path.resolve(this.getOutputDirectory(), TS_CONFIG_FILENAME);
+  }
+
   public isPrettyLogs(): boolean {
     return this.config.prettyLogs;
   }
@@ -103,4 +131,39 @@ export class Context {
     this.config.logLevel = logLevel;
     logger.resetLogger();
   }
+
+  public setOverwriteFiles(overwrite: boolean) {
+    this.config.overwriteFiles = overwrite;
+  }
+
+  public isOverwriteFilesEnabled(): boolean {
+    return this.config.overwriteFiles;
+  }
+
+  public setOpenApiUri(uri: string) {
+    this.config.openApiUri = uri;
+  }
+
+  public getOpenApiUri(): string {
+    if (!this.config.openApiUri) {
+      throw new Error("Open API URI is null");
+    }
+    return this.config.openApiUri;
+  }
+
+/**
+ * this function is added because the variable `__dirname` points to two different
+ * locations depending on how the code is being run.
+ * If the code is run via tests, it points to the directory in typescript code layout
+ * otherwise it points to the genenrated javascript directory
+ *
+ * @returns the correct parent directory containing templates
+ */
+public getTemplatesDirectory(): string {
+  if (fs.existsSync(path.resolve(__dirname, "../../templates"))) {
+    return path.resolve(__dirname, "../../templates");
+  } else {
+    return path.resolve(__dirname, "../../../templates");
+  }
+};
 }
