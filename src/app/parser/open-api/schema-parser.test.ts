c87534a70d3ac60c359e00148404c9e2e7b11d94
m-Bilal <mohd.bilal1996@gmail.com>
2024-05-23 17:54:16 +0530

    schema parser to determine whether a type requires allow relaxed types annotation

---
+++ src/app/parser/open-api/schema-parser.test.ts
@@ -0,0 +1,119 @@
+import * as context from "../../context";
+import * as assert from "assert";
+import path from "path";
+import * as apiGenerator from "../../generator/api-ts-generator";
+import * as schemaParser from "./schema-parser";
+import * as fs from "fs";
+
+import { SchemaTypeObject } from "./types";
+
+type RelaxedTypeCheck = {
+  schemaRef: string,
+  requiresRelaxedTypeJsDocTag: boolean,
+};
+
+const tests: {
+  name: string,
+  openApiFile: string,
+  goldenFile: string,
+  expected?: Map<string, boolean>, // using a map instead of RelaxedTypeCheck[] so that ordering can be ignored
+}[] = [
+  {
+    name: "Petstore",
+    openApiFile: "petstore.yaml",
+    goldenFile: "petstore.json"
+  },
+  {
+    name: "DemoBlogApi",
+    openApiFile: "demo-blog-api.json",
+    goldenFile: "demo-blog-api.json"
+  },
+    {
+    name: "Gitlab",
+    openApiFile: "gitlab.json",
+    goldenFile: "gitlab.json",
+  },
+  {
+    name: "Instagram",
+    openApiFile: "instagram.json",
+    goldenFile: "instagram.json",
+  },
+  {
+    name: "Geomag",
+    openApiFile: "geomag.json",
+    goldenFile: "geomag.json",
+  },
+    {
+    name: "GoogleHome",
+    openApiFile: "google-home.json",
+    goldenFile: "google-home.json",
+  },
+  {
+    name: "GoogleAdsense",
+    openApiFile: "google-adsense.json",
+    goldenFile: "google-adsense.json",
+  },
+  {
+    name: "CircleCI",
+    openApiFile: "circleci.json",
+    goldenFile: "circleci.json",
+  },
+  {
+    name: "aws-autoscaling",
+    openApiFile: "aws-autoscaling.json",
+    goldenFile: "aws-autoscaling.json",
+  },
+  {
+    name: "adobe",
+    openApiFile: "adobe.json",
+    goldenFile: "adobe.json",
+  },
+  {
+    name: "Kubernetes",
+    openApiFile: "kubernetes.json",
+    goldenFile: "kubernetes.json",
+  },
+];
+
+describe("schema-parser", async() => {
+  for (const testCase of tests) {
+    before(function() {
+      testCase.openApiFile = path.resolve(__dirname, "../../../../tests/test-data/open-api-docs", testCase.openApiFile);
+      testCase.goldenFile = path.resolve(__dirname, "./test-data/golden-files/schema-parser-tests/", testCase.goldenFile);
+      testCase.expected = new Map<string, boolean>();
+      try {
+        const expectedArray: RelaxedTypeCheck[] = JSON.parse(fs.readFileSync(testCase.goldenFile).toString());
+        expectedArray.forEach((element) => {
+          testCase.expected!.set(element.schemaRef, element.requiresRelaxedTypeJsDocTag);
+        });        
+      } catch (e) {
+        
+      };
+    })
+
+    it(testCase.name, async() => {
+      const generatedCode = await apiGenerator.generateApiTsCode(testCase.openApiFile);
+      const parser = schemaParser.getParsedSchema(generatedCode.typeNames, generatedCode.schemaComponents);
+
+      // console.log('parser.mappings', parser.mappings);
+
+      const got: Map<string, boolean> = new Map<string, boolean>();
+      const gotTyped: RelaxedTypeCheck[] = [];
+      parser.mappings.refToSchemaMap.forEach((value, key) => {
+        got.set(key,  value._requiresRelaxedTypeJsDocTag ?? false);
+
+        gotTyped.push({
+          schemaRef: key,
+          requiresRelaxedTypeJsDocTag: value._requiresRelaxedTypeJsDocTag ?? false,
+        });
+
+        // console.log(`$ref: ${key}; isRelaxedType: ${value._requiresRelaxedTypeJsDocTag}`);
+      });
+
+        assert.deepEqual(got, testCase.expected);
+
+        // uncomment to write to golden file
+        // fs.writeFileSync(testCase.goldenFile, JSON.stringify(gotTyped));
+    });
+  }
+});
--- src/app/parser/open-api/types.ts
+++ src/app/parser/open-api/types.ts
@@ -3,6 +3,9 @@ export type Schema = {
   typeName: string;
   componentName: string;
   rawTypeData: SchemaProperty | undefined;
+
+  // this variable is calculated by this package
+  _requiresRelaxedTypeJsDocTag?: boolean;
 };
 
 export type SchemaProperty =
@@ -11,12 +14,13 @@ export type SchemaProperty =
   | SchemaTypeObject
   | SchemaTypeAllOf
   | SchemaTypeRef
+  | SchemaTypeContent
   | SchemaTypeRawArg;
 
 export type SchemaTypeObject = {
   type: "object";
   required: string[] | undefined;
-  properties: Record<string, SchemaProperty> | undefined;
+  properties: Map<string, SchemaProperty> | undefined;
   example: string | undefined;
   description: string | undefined;
 };
@@ -51,7 +55,7 @@ export type SchemaTypeAllOf = {
 };
 
 export type SchemaTypeContent = {
-  content: Record<string, SchemaProperty>;
+  content: Map<string, SchemaProperty>;
   example: string | undefined;
   description: string | undefined;
   required: boolean | undefined;
@@ -72,3 +76,119 @@ export type SchemaTypeRawArg = {
   nullable: boolean | undefined;
   schema: SchemaProperty;
 };
+
+export function schemaPropertyIsTypeScaler(property: any): property is SchemaTypeScalar {
+  return (
+    property.type &&
+    (property.type === "integer" ||
+      property.type === "string" ||
+      property.type === "boolean" ||
+      property.type === "number")
+  );
+}
+
+export function schemaPropertyIsTypeObject(property: any): property is SchemaTypeObject {
+  return property.type && property.type === "object";
+}
+
+export function schemaPropertyIsTypeArray(property: any): property is SchemaTypeArray {
+  return property.type && property.type === "array" && property.items;
+}
+
+export function schemaPropertyIsTypeAllOf(property: any): property is SchemaTypeAllOf {
+  return property.allOf !== null && property.allOf !== undefined;
+}
+
+export function schemaPropertyIsTypeRef(property: any): property is SchemaTypeRef {
+  return property.$ref !== null && property.$ref !== undefined;
+}
+
+export function schemaPropertyIsTypeContent(property: any): property is SchemaTypeContent {
+  return property.content !== null && property.content !== undefined;
+}
+
+export function schemaPropertyIsTypeRawArg(property: any): property is SchemaTypeRawArg {
+  return property.in && property.name && property.schema;
+}
+
+export function schemaPropertyIsEnum(property: any): boolean {
+  return (
+    (schemaPropertyIsTypeScaler(property) &&
+      property.enum &&
+      Array.isArray(property.enum) &&
+      property.enum.length > 0) ??
+    false
+  );
+}
+
+export function getSchemaPropertyChildren(property: SchemaProperty): SchemaProperty[] {
+  if (schemaPropertyIsTypeObject(property)) {
+    console.log('getSchemaPropertyChildren: schemaProperty is object');
+    return getSchemaTypeObjectChildern(property);
+  } else if (schemaPropertyIsTypeArray(property)) {
+    console.log('getSchemaPropertyChildren: schemaProperty is array');
+    return getSchemaTypeArrayChildern(property);
+  } else if (schemaPropertyIsTypeContent(property)) {
+    console.log('getSchemaPropertyChildren: schemaProperty is content');
+    return getSchemaTypeContentChildren(property);
+  } else if (schemaPropertyIsTypeRawArg(property)) {
+    console.log('getSchemaPropertyChildren: schemaProperty is is raw args');
+    return getSchemaTypeRawArgsChildren(property);
+  } else if (schemaPropertyIsTypeAllOf(property)) {
+    console.log('getSchemaPropertyChildren: schemaProperty is all of');
+    return getSchemaTypeAllOfChildren(property);
+  }
+  console.log('getSchemaPropertyChildren: schemaProperty is likely a scaler or ref. returning empty array');
+  return [];
+}
+
+export function getSchemaTypeObjectChildern(schemaProperty: SchemaTypeObject): SchemaProperty[] {
+  if (schemaProperty && schemaProperty.properties) {
+    return Object.values(schemaProperty.properties);
+  } else {
+    return [];
+  }
+}
+
+export function getSchemaTypeArrayChildern(schemaProperty: SchemaTypeArray): SchemaProperty[] {
+  if (schemaProperty && schemaProperty.items) {
+    return [schemaProperty.items];
+  } else {
+    return [];
+  }
+}
+
+export function getSchemaTypeContentChildren(schemaProperty: SchemaTypeContent): SchemaProperty[] {
+  if (schemaProperty && schemaProperty.content) {
+    return Object.values(schemaProperty.content);
+  } else {
+    return [];
+  }
+}
+
+export function getSchemaTypeRawArgsChildren(schemaProperty: SchemaTypeRawArg): SchemaProperty[] {
+  if (schemaProperty && schemaProperty.schema) {
+    return [schemaProperty.schema];
+  }
+  return [];
+}
+
+export function getSchemaTypeAllOfChildren(schemaProperty: SchemaTypeAllOf): SchemaProperty[] {
+  if (schemaProperty && schemaProperty.allOf) {
+    return schemaProperty.allOf;
+  }
+  return [];
+}
+
+/**
+ * Checks whether a type should be marked as a RelaxedType
+ * More on RelaxedTypes: https://github.com/hasura/ndc-nodejs-lambda?tab=readme-ov-file#relaxed-types
+ */
+export function schemaPropertyIsRelaxedType(schemaProperty: SchemaProperty) {
+  if (schemaPropertyIsTypeScaler(schemaProperty)) {
+    if (schemaPropertyIsEnum(schemaProperty)) {
+      return true;
+    }
+  }
+  return false;
+}
