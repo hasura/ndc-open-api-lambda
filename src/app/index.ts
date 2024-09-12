f2adfdaf77732defffa570b550c376651838b617
m-Bilal <mohd.bilal1996@gmail.com>
2024-09-12 14:10:23 +0530

    removed comma

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,13 +10,13 @@ export async function runApp(args: types.GenerateCodeInput) {
   if (!context.getInstance().isOverwriteFilesEnabled()) {
     if (fs.existsSync(context.getInstance().getApiFilePath())) {
       logger.fatal(
-        `Error: overwriting is disabled and api.ts file already exists at ${context.getInstance().getApiFilePath()}. To enable file overwrite, please set 'NDC_OAS_FILE_OVERWRITE' environment variable to true, or, use the --overwrite flag`,
+        `Error: overwriting is disabled and api.ts file already exists at ${context.getInstance().getApiFilePath()}. To enable file overwrite, please set 'NDC_OAS_FILE_OVERWRITE' environment variable to true, or use the --overwrite flag`,
       );
       exit(0);
     }
     if (fs.existsSync(context.getInstance().getFunctionsFilePath())) {
       logger.fatal(
-        `Error: overwriting is disabled and functions.ts file already exists at ${context.getInstance().getFunctionsFilePath()}. To enable file overwrite, please set 'NDC_OAS_FILE_OVERWRITE' environment variable to true, or, use the --overwrite flag`,
+        `Error: overwriting is disabled and functions.ts file already exists at ${context.getInstance().getFunctionsFilePath()}. To enable file overwrite, please set 'NDC_OAS_FILE_OVERWRITE' environment variable to true, or use the --overwrite flag`,
       );
       exit(0);
     }
