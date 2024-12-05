6defcebd29bf451c29be4c91f26efb9facf94f8d
m-Bilal <mohd.bilal1996@gmail.com>
2024-12-05 19:22:28 +0530

    walk ts file and get all variables

--- src/app/parser/typescript/walk.ts
+++ src/app/parser/typescript/walk.ts
@@ -1,4 +1,5 @@
 import * as ts from "ts-morph";
+import * as logger from "../../../util/logger"
 
 export type FunctionWithTagsAsStringArray = {
   function: ts.FunctionDeclaration;
@@ -75,4 +76,74 @@ export function getFunctionName(node: ts.FunctionDeclaration) {
 
 export function isSavedFunction(func: FunctionWithTagsAsStringArray): boolean {
   return func.tags.indexOf("save") > -1;
+}
+
+export function isNodeSaved(node: ts.Node): boolean {
+  const allJSDocs = getAllJSDocs(node);
+  if (!allJSDocs) {
+    return false;
+  }
+
+  for (let i =0; i < allJSDocs.length; i++) {
+    const jsDoc = allJSDocs[i];
+    if (!jsDoc) {
+      continue;
+    }
+    const allTags = getAllJSDocsTagsAsStringArray(jsDoc);
+    if (allTags.includes("save")) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * get all constants and variables declared in the global scope of a TS source file
+ * @param tsSourceFile 
+ * @returns 
+ */
+export function getAllVariables(tsSourceFile: ts.SourceFile): ts.VariableStatement[] {
+  const allVariables: ts.VariableStatement[] = [];
+  if (!tsSourceFile) {
+    return allVariables;
+  }
+  tsSourceFile.forEachChild((node) => {
+    if (ts.Node.isVariableStatement(node)) {
+      allVariables.push(node as ts.VariableStatement);
+    }
+  });
+  return allVariables;
+}
+
+/**
+ * returns all constants and variables as a map of variable name to ts.VariableStatement
+ * @param tsSourceFile 
+ * @returns 
+ */
+export function getAllVariablesMap(tsSourceFile: ts.SourceFile): Map<string, ts.VariableStatement> {
+  const allVariables = getAllVariables(tsSourceFile);
+  const allVariablesMap: Map<string, ts.VariableStatement> = new Map();
+  allVariables.forEach((variable) => {
+    const variableName = getVariableName(variable);
+    if (!variableName) {
+      return;
+    }
+    allVariablesMap.set(variableName, variable);
+  });
+  return allVariablesMap;
+}
+
+
+export function getVariableName(node: ts.VariableStatement) {
+  if (!node || !node.getDeclarations()) {
+    return undefined;
+  }
+  if (node.getDeclarations().length === 0) {
+    logger.error('Variable name not found in declaration: ', node.getFullText());
+    return undefined;
+  } else if (node.getDeclarations().length > 1) {
+    logger.warn('Multiple variable declarations are not supported for `@save`. Found in node: ', node.getFullText());
+    return undefined;
+  }
+  return node.getDeclarations()[0]!.getName();
 }
